8797b4a664e131b14de539280c904ece4cf65000
dlswo <dlswo4456@naver.com>
2019-01-25 12:18:45 +0900

    BillBoard ControllPanel Reducer

---
+++ src/components/ControllPanel.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+
+class ControllPanel extends Component{
+
+    changeColor = (e) => {
+        const codeValue = e.target.getAttribute("data-code")
+        console.log(codeValue)
+        const actionData = {
+            color:codeValue
+        }
+        this.props.dispatch({type:"change", data:actionData})
+    }
+
+    render(){
+        return(
+            <div>
+                <h3>ControllPanel</h3>
+                <button data-code="red" onClick={this.changeColor}>Red</button>
+                <button data-code="green" onClick={this.changeColor}>Green</button>
+                <button data-code="blue" onClick={this.changeColor}>Blue</button>
+            </div>
+        )
+    }
+
+}
+export default connect()(ControllPanel)
